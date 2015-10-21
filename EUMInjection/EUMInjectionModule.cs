eeaa9bf160af2708b034706c7fd4bc6193185841
soickm <pedro.pacheco@soickm,com>
2015-10-21 12:46:25 -0600

    New Commit

---
+++ EUMInjection/EUMInjectionModule.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Web;
+using System.IO;
+using System.Diagnostics;
+using System.Configuration;
+using System.Reflection;
+using System.Net;
+
+/// <summary>
+/// This is a custom module that inserts 
+/// </summary>
+
+public class EUMInjectionModule : IHttpModule
+{
+    string sSource = "AppDynamics HTTP Module";
+    string sLog = "Application";
+    Boolean reference;                  // Inline or reference script
+    String uri;                         // URI to ADRUM.js
+
+    public EUMInjectionModule()
+    {
+        if (!EventLog.SourceExists(sSource))
+            EventLog.CreateEventSource(sSource, sLog);
+    }
+
+    public String ModuleName
+    {
+        get { return "EUMInjeectionModule"; }
+    }
+
+    public void Init(HttpApplication application)
+    {
+        EventLog.WriteEntry(sSource, "HTTP Module registration", EventLogEntryType.Information);
+
+        EventLog.WriteEntry(sSource, "Loading configuration from file:" + AssemblyDirectory + "\\EUMInjection.dll");
+        EventLog.WriteEntry(sSource, "HTTP Post Request", EventLogEntryType.Information);
+        Configuration appConfig = ConfigurationManager.OpenExeConfiguration(AssemblyDirectory+ "\\EUMInjection.dll");
+
+        reference = Convert.ToBoolean(appConfig.AppSettings.Settings["Reference"].Value);
+        uri = Convert.ToString(appConfig.AppSettings.Settings["AdrumURI"].Value);
+
+        EventLog.WriteEntry(sSource, "Reference:" + reference.ToString());
+        EventLog.WriteEntry(sSource, "URI:" + uri);
+
+        application.PostRequestHandlerExecute += (new EventHandler(Application_PostRequest));
+    }
+    
+    private void Application_PostRequest(Object source, EventArgs e)
+    {
+        EventLog.WriteEntry(sSource, "HTTP Post Request", EventLogEntryType.Information);
+
+        HttpApplication currentApplication = (HttpApplication)source;
+
+        if (currentApplication.Response.ContentType.Contains("text/html"))
+        {
+
+            string insertionPointString = "</head>";
+
+            // Substitute the Response.Filter with custom stream implementation
+            currentApplication.Response.Filter = new PostRenderModifier(currentApplication.Response.Filter, insertionPointString);
+            ((PostRenderModifier)currentApplication.Response.Filter).StringToInsert = (reference == true) ? Reference() : InLine();
+        }
+    }
+
+    private string Reference()
+    {
+        return String.Format("\n<script>window['adrum-start-time'] = new Date().getTime();</script><script src=\"{0}\"></script>\n", uri);
+    }
+
+    private string InLine()
+    {
+        string javascript;
+        using (WebClient client = new WebClient())
+        {
+            javascript = client.DownloadString(uri);
+        }
+        return String.Format("\n<script>window['adrum-start-time'] = new Date().getTime();</script><script type=\"text/javascript\">{0}</script>\n", javascript.ToString());
+    }
+
+    public static string AssemblyDirectory
+    {
+        get
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(path);
+        }
+    }
+
+    public void Dispose() { }
+}
+
---
+++ EUMInjection/PostRenderModifier.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+    /// <summary>
+    /// This is a custom stream implementation that inserts a set of bytes into the byte stream.
+    /// </summary>
+    public class PostRenderModifier : Stream
+    {
+
+        // Passed in by the HTTPModule for processing
+        public string StringToInsert = string.Empty;
+
+        // Set of bytes, where the StringToInsert will be inserted
+        private byte[] m_InsertionPointUTF8Bytes;
+
+        // The output stream that will include inserted data
+        private Stream m_ResponseOutputStream;
+
+        /// <summary>
+        /// Consturctor
+        /// </summary>
+        /// <param name="OutputStream">The response output stream</param>
+        /// <param name="InsertionPoint">Token where the data will be inserted</param>
+        public PostRenderModifier(Stream OutputStream, string InsertionPoint)
+        {
+            m_InsertionPointUTF8Bytes = Encoding.UTF8.GetBytes(InsertionPoint);
+            m_ResponseOutputStream = OutputStream;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="OutputStream">The response output stream</param>
+        /// <param name="InsertionPointUTF8">Token where the data will be inserted</param>
+        public PostRenderModifier(Stream OutputStream, byte[] InsertionPointUTF8)
+        {
+            m_InsertionPointUTF8Bytes = InsertionPointUTF8;
+            m_ResponseOutputStream = OutputStream;
+        }
+
+
+        // The following members of Stream must be overridden. Simply delegate to the contained stream
+        public override bool CanRead { get { return true; } }
+        public override bool CanSeek { get { return true; } }
+        public override bool CanWrite { get { return true; } }
+        public override long Length { get { return 0; } }
+        public override long Position { get { return m_ResponseOutputStream.Position; } set { m_ResponseOutputStream.Position = value; } }
+        public override long Seek(long offset, System.IO.SeekOrigin direction) { return m_ResponseOutputStream.Seek(offset, direction); }
+        public override void SetLength(long length) { m_ResponseOutputStream.SetLength(length); }
+        public override void Close() { m_ResponseOutputStream.Close(); }
+        public override void Flush() { m_ResponseOutputStream.Flush(); }
+        public override int Read(byte[] buffer, int offset, int count) { return m_ResponseOutputStream.Read(buffer, offset, count); }
+
+        /// <summary>
+        /// Writes modified data to response output stream
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            // Validate arguments
+            if (null == buffer)
+                throw new ArgumentNullException("buffer", "buffer cannot be null");
+            if (0 > offset)
+                throw new ArgumentOutOfRangeException("offset", "offset cannot be negative");
+            if (0 > count)
+                throw new ArgumentOutOfRangeException("count", "offset cannot be negative");
+
+            // Modify the stream if we have StringToInsert
+            if ((0 < count) &&
+               (0 != StringToInsert.Length))
+            {
+                //Find if the current buffer contains the set of bytes used as an insertion token
+                int insertionTokenIndex = PostRenderModifier.LastIndexOf(buffer, offset, count, this.m_InsertionPointUTF8Bytes);
+
+                if (insertionTokenIndex < 0)
+                {
+                    // insertion token was not found, output the original bytes to the response stream
+                    m_ResponseOutputStream.Write(buffer, offset, count);
+                }
+                else
+                {
+                    // Insertion token was found.  the steps are:
+                    // 1) write bytes before the token
+                    // 2) write the StringToInsert
+                    // 3) write bytes including and after the token
+
+                    if (insertionTokenIndex > 0)
+                    {
+                        // 1. write bytes before the impression token
+                        m_ResponseOutputStream.Write(buffer, offset, insertionTokenIndex - offset);
+                    }
+
+                    // 2. write the StringToInsert
+                    // convert the string to bytes for output
+                    byte[] insertionStringUTF8Bytes = Encoding.UTF8.GetBytes(StringToInsert);
+                    m_ResponseOutputStream.Write(insertionStringUTF8Bytes, 0, insertionStringUTF8Bytes.Length);
+
+                    if (count - insertionTokenIndex > 0)
+                    {
+                        // 3. write bytes including and after the insertion token
+                        m_ResponseOutputStream.Write(buffer, insertionTokenIndex, count - insertionTokenIndex);
+                    }
+                }
+            }
+            else // Do nothing and delegate
+                m_ResponseOutputStream.Write(buffer, offset, count);
+        }
+
+
+        /// <summary>
+        /// Calculates the index of the last occurrence of a series of bytes in another series of bytes.
+        /// </summary>
+        /// <param name="buffer">The bytes to search</param>
+        /// <param name="bufferStartIdx">The offset where the buffer starts</param>
+        /// <param name="bufferLength">The size of the data in the buffer</param>
+        /// <param name="findSeries">The series of bytes to locate in the buffer</param>
+        /// <returns>The index of the last occurrence of the series of bytes, or -1 if it could not be found</returns>
+        private static int LastIndexOf(byte[] buffer, int bufferStartIdx, int bufferLength, byte[] findSeries)
+        {
+            bool found = false; // true if we found the series in the buffer
+            int findIdx; // index into the findSeries
+            int srcLocalIdx; // used so we don't mess up the index used by the outer loop
+            int srcIdx = bufferStartIdx + bufferLength; // the index is really 1 less, but the first while loop condition does the necessary decrement
+            int srcLeftMostSearchIdx; // the left most index in the buffer were a match could happen
+
+            // Loop backwards through the buffer.  For each index in the buffer, loop backwards through the findSeries.
+            // If the series does not match, exit the inner loop and try again with the next backwards starting point in 
+            // the buffer.  If the series matches, set found to true so we exit out of the loop.  Return the index.
+
+            srcLeftMostSearchIdx = bufferStartIdx + findSeries.Length;
+
+            while (!found && srcIdx-- >= srcLeftMostSearchIdx)
+            {
+                srcLocalIdx = srcIdx;
+                for (findIdx = findSeries.Length - 1; findIdx >= 0; --findIdx)
+                {
+                    if (findSeries[findIdx] == buffer[srcLocalIdx])
+                    {
+                        if (0 == findIdx)
+                        {
+                            found = true;
+                        }
+                        else
+                        {
+                            --srcLocalIdx;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+            }
+            return found ? srcIdx - findSeries.Length + 1 : -1;
+        }
+    }
